afd329aa493813c5f85492ec6582898a363b2ac4
Mikołaj Wargowski <mikoajw20@gmail.com>
2020-08-26 17:56:21 +0200

    ✨ User Registration Domain

---
+++ src/core/user-access/user-registration/events/user-registered.domain-event.ts
@@ -0,0 +1,9 @@
+import { DomainEvent } from '@core/shared/domain-event';
+
+export const ACCOUNT_REGISTERED_DOMAIN_EVENT = 'user-registration/registered';
+
+export class UserRegisteredDomainEvent extends DomainEvent {
+  constructor(public readonly email: string, public readonly username: string) {
+    super(ACCOUNT_REGISTERED_DOMAIN_EVENT);
+  }
+}
